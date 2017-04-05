1a9aabea46a2ce398ca20be91fb8eef26dfbf311
CarnivalO <xmwjn15@gmail.com>
2017-04-05 12:41:57 +0800

    增加卡片切换，增加删除

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-## vue2.0及axios的购物车
+# vue2.0及axios的购物车
 
+## 
+v-model  
+v-text  
+v-show  
+v-if  
+v-bind  
+v-for  
+v-on 
+
+过滤器
+computed
+
+
+列表 v-for
+价格转换 过滤器
+form表单 v-model 
+操作 v-on
+样式 v-bind
+没有变量的话 使用vue set
--- js/address.js
+++ js/address.js
@@ -1,37 +1,68 @@
 new Vue({
-    el:'.container',
-    data:{
-        limitNumber:3,        
-        addressList:[],
-        loadFlag:true
+    el: '.container',
+    data: {
+        limitNumber: 3,
+        addressList: [],
+        loadFlag: true,
+        currentIndex: 0,
+        shippingMethod: 1,
+        delFlag: false,
+        modFlag:false
     },
-    mounted:function(){
-        this.$nextTick(function(){
+    mounted: function () {
+        this.$nextTick(function () {
             this.getAddressList();
         })
     },
-     computed:{
-        filteredAddress:function(){
-            return this.addressList.slice(0,this.limitNumber);
+    computed: {
+        filteredAddress: function () {
+            return this.addressList.slice(0, this.limitNumber);
         }
     },
-    methods:{
-        getAddressList:function(){
+    methods: {
+        getAddressList: function () {
             var _this = this;
-             axios.get("data/address.json").then(response => {
+            axios.get("data/address.json").then(response => {
                 var res = response.data;
-               if(res.status == "1"){
-                   _this.addressList = res.result;
-               }
+                if (res.status == "1") {
+                    _this.addressList = res.result;
+                }
             });
         },
-        loadAll:function(loadFlag){
+        loadAll: function (loadFlag) {
             this.loadFlag = !this.loadFlag;
-            if(this.loadFlag){
-            this.limitNumber = 3;
-        }else{
-            this.limitNumber = this.addressList.length;
+            if (this.loadFlag) {
+                this.limitNumber = 3;
+            } else {
+                this.limitNumber = this.addressList.length;
             }
-        }
+        },
+        setDefault: function (addressId) {
+            this.addressList.forEach(function (item, index) {
+                if (item.addressId == addressId) {
+                    item.isDefault = true;
+                } else {
+                    item.isDefault = false;
+                }
+            });
+        },
+        delConfirm: function (index) {
+            this.delFlag = true;
+            this.currentIndex = index;
+        },
+        delAddress: function () {
+            this.addressList.splice(this.currentIndex, 1);
+            this.delFlag = false;
+            //axios.get() 通过后台删除
+        },
+        modConfirm:function (index){
+            this.modFlag = true;
+            this.currentIndex = index;
+        },
+         modAddress: function () {
+            this.addressList.splice(this.currentIndex, 1);
+            this.delFlag = false;
+            //axios.get() 通过后台删除
+        },
     }
 })
